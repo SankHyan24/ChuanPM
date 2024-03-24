845541a2937e02d354eee021671b5d32b65a031a
Charles Sun <2209339564@qq.com>
2024-03-24 14:41:16 +0800

    添加新的eval方式，不使用cosine值，只求得brdf

--- scripts/sc.py
+++ scripts/sc.py
@@ -3,7 +3,7 @@ from falcor import *
 
 def render_graph_DefaultRenderGraph():
     g = RenderGraph('DefaultRenderGraph')
-    g.create_pass('VBufferSC', 'VBufferSC', {'outputSize': 'Default', 'samplePattern': 'Center', 'sampleCount': 16, 'useAlphaTest': True, 'adjustShadingNormals': True, 'forceCullMode': False, 'cull': 'Back', 'useTraceRayInline': False, 'useDOF': True, 'specRoughCutoff': 0.5, 'recursionDepth': 10})
+    g.create_pass('VBufferSC', 'VBufferSC', {'outputSize': 'Default', 'samplePattern': 'Center', 'sampleCount': 16, 'useAlphaTest': True, 'adjustShadingNormals': True, 'forceCullMode': False, 'cull': 'Back', 'useTraceRayInline': False, 'useDOF': True, 'specRoughCutoff': 0.1, 'recursionDepth': 10})
     g.create_pass('ToneMapper', 'ToneMapper', {'outputSize': 'Default', 'useSceneMetadata': True, 'exposureCompensation': 0.0, 'autoExposure': False, 'filmSpeed': 100.0, 'whiteBalance': False, 'whitePoint': 6500.0, 'operator': 'Aces', 'clamp': True, 'whiteMaxLuminance': 1.0, 'whiteScale': 11.199999809265137, 'fNumber': 1.0, 'shutter': 1.0, 'exposureMode': 'AperturePriority'})
     g.create_pass('RTPM', 'RTPM', {})
     g.add_edge('VBufferSC.vbuffer', 'RTPM.vbuffer')
